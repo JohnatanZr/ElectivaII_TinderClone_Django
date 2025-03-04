217419aa21e84add7a7cdc5430744624df8861bb
Johnatan Duvan Bedoya Gomez <jonadupe@outlook.com>
2025-03-04 00:42:52 -0500

    Divided responsibility

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { signup, login } = require("../controllers/authControllers");
 const { validLoginUsers, isValidLogin } = require("../validators/loginValidators");
 const { validSignupUsers, isValidSignup } = require("../validators/SignupValidators");
+const { signup } = require("../controllers/authControllers/signup");
+const { login } = require("../controllers/authControllers/login");
 
 const router = express.Router();
 
-router.post("/signup", validSignupUsers, isValidSignup, signup);
+router.post("/signup", validSignupUsers, isValidSignup, signup );
 router.post("/login", validLoginUsers, isValidLogin, login);
 
 module.exports = router;
